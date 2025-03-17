4ceb9efb7dd83ad29ef17494dd9e1b84c6960948
Rishi <avviare.rishi@gmail.com>
2025-03-17 09:17:33 +0530

    project initialization

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 class="font-bold text-3xl underline">
+        Hello world!
+      </h1>
+    </>
+  )
+}
+
+export default App
